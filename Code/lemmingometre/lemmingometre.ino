84ced2849d318265ede70ac71be704ed8969d304
NathanBerube <naber260@ulaval.ca>
2023-07-12 11:11:32 -0400

    File open/close for every writing v1
    To save data in a file, it needs to be closed. It is likely that the Wemos will reset before the acquisition is over. If the file is only closed after the acquisition delay, no file will be created if a reset is triggered before the acquisition delay.

--- Code/lemmingometre/lemmingometre.ino
+++ Code/lemmingometre/lemmingometre.ino
@@ -12,7 +12,9 @@ int chipSelect = D8;  //SD card
 int obstacleSensor = D4;
 
 ///// DATA VARIABLES /////
+int obstacle_acquisition_time = 30000;
 int obstacleValue;
+DateTime now;
 
 ///// MAIN CODE /////
 void setup() {
@@ -23,20 +25,23 @@ void setup() {
 
   setupRTC();
   setupSD();
-
+  setFileName();
+  writeFileHeading();
 
   // Data logging
   unsigned long startTime = millis();
-  startLogFile();
-  while (millis() - startTime < 1000) {
-    readObstacleSensor();
-    getTimeStamp();
+
+  while (millis() - startTime < obstacle_acquisition_time) {
+    now = rtc.now();
+    printTimeStamp();
+
+    obstacleValue = readObstacleSensor();
+    printObstacleSensorReading();
+
+    writeTimeAndCountInFile();
   }
 
-  endLogFile();
-  readLogFile();
   Serial.println("Going to sleep");
-
   // Wemos goes to sleep
   ESP.deepSleep(0);
 }
@@ -44,19 +49,13 @@ void setup() {
 void loop() {
 }
 
-void readObstacleSensor() {
-  obstacleValue = !digitalRead(obstacleSensor);
-  Serial.print("Lemming count: ");
-  myFile.print("Lemming count: ");
-  Serial.println(obstacleValue);
-  myFile.println(obstacleValue);
-}
+
 
 void setupRTC() {
-#ifndef ESP8266
-  while (!Serial)
-    ;  // wait for serial port to connect. Needed for native USB
-#endif
+  #ifndef ESP8266
+    while (!Serial)
+      ;  // wait for serial port to connect. Needed for native USB
+  #endif
   if (!rtc.begin()) {
     Serial.println("RTC initialization failed");
     Serial.flush();
@@ -74,51 +73,107 @@ void setupSD() {
   Serial.println("SD card initialization done.");
 }
 
-void getTimeStamp() {
-  rtc.start();
+void setFileName() {
   DateTime now = rtc.now();
-
-  myFile.print(now.year(), DEC);
-  Serial.print(now.year(), DEC);
-  myFile.print('/');
-  Serial.print('/');
-  myFile.print(now.month(), DEC);
-  Serial.print(now.month(), DEC);
-  myFile.print('/');
-  Serial.print('/');
-  myFile.print(now.day(), DEC);
-  Serial.print(now.day(), DEC);
-  myFile.print(" ");
-  Serial.print(" ");
-  myFile.print(now.hour(), DEC);
-  Serial.print(now.hour(), DEC);
-  myFile.print(':');
-  Serial.print(':');
-  myFile.print(now.minute(), DEC);
-  Serial.print(now.minute(), DEC);
-  myFile.print(':');
-  Serial.print(':');
-  myFile.print(now.second(), DEC);
-  Serial.print(now.second(), DEC);
-  myFile.println();
-  Serial.println();
+  snprintf(filename, 50, "%d-%d-%d-%d-%d-%d.txt", now.year(), now.month(), now.day(), now.hour(), now.minute(), now.second());
 }
 
-void startLogFile() {
-  DateTime now = rtc.now();
-  
-  snprintf(filename, 50, "%d-%d-%d-%d-%d-%d.txt", now.year(), now.month(), now.day(), now.hour(), now.minute(), now.second());
+
+void writeFileHeading() {
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
   myFile = SD.open(filename, FILE_WRITE);
 
   // if the file opened okay, write to it:
   if (myFile) {
-    // Serial.print("Writing to test.txt...");
-    // myFile.println("testing 1, 2, 4.");
+    Serial.print("Writing heading ...");
+    myFile.print("Time stamp");
+    myFile.print(",");
+    myFile.println("Lemming count");
+    myFile.close();
+    Serial.println("done.");
   } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
+  // if the file didn't open, print an error:
+    Serial.println("error opening file");
   }
 }
+
+void printTimeStamp(){
+  //myFile.print(now.year(), DEC);
+  Serial.print(now.year(), DEC);
+  //myFile.print('/');
+  Serial.print('/');
+  //myFile.print(now.month(), DEC);
+  Serial.print(now.month(), DEC);
+  //myFile.print('/');
+  Serial.print('/');
+ // myFile.print(now.day(), DEC);
+  Serial.print(now.day(), DEC);
+ // myFile.print(" ");
+  Serial.print(" ");
+ // myFile.print(now.hour(), DEC);
+  Serial.print(now.hour(), DEC);
+ // myFile.print(':');
+  Serial.print(':');
+  //myFile.print(now.minute(), DEC);
+  Serial.print(now.minute(), DEC);
+ // myFile.print(':');
+  Serial.print(':');
+ // myFile.print(now.second(), DEC);
+  Serial.print(now.second(), DEC);
+  //myFile.println();
+  Serial.println();
+}
+
+int readObstacleSensor() {
+  return !digitalRead(obstacleSensor);
+}
+
+void printObstacleSensorReading() {
+  Serial.print("Lemming count: ");
+  Serial.println(obstacleValue);
+}
+
+
+
+void writeTimeAndCountInFile() {
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+    }
+  myFile = SD.open(filename, FILE_WRITE);
+
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.print("Writing time stamp and count to file ...");
+    myFile.print(now.year(), DEC);
+    myFile.print('/');
+    myFile.print(now.month(), DEC);
+    myFile.print('/');
+    myFile.print(now.day(), DEC);
+    myFile.print(" ");
+    myFile.print(now.hour(), DEC);
+    myFile.print(':');
+    myFile.print(now.minute(), DEC);
+    myFile.print(':');
+    myFile.print(now.second(), DEC);
+    myFile.print(",");
+    myFile.println(obstacleValue);
+
+    myFile.close();
+    Serial.println("done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening file");
+  }
+}
+
+
+
+
 
 void readLogFile() {
   // re-open the file for reading:
@@ -135,6 +190,6 @@ void readLogFile() {
     Serial.println("error opening file");
   }
 }
-void endLogFile() {
-  myFile.close();
-}
+
+
+
