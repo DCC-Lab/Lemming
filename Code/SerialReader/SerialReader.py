913f78facc569091e13971a891d206c8c7ff063a
Ludovic Garon <ludovicgaron54@gmail.com>
2024-06-05 16:12:03 -0400

    added ui and button, there is a dependency bug

--- Code/SerialReader/SerialReader.py
+++ Code/SerialReader/SerialReader.py
@@ -2,43 +2,55 @@ import serial
 import time
 import os
 import numpy as np
+import tkinter as tk
+from tkinter import filedialog as fd
 
-# Open a serial connection (modify 'COMX' to your Arduino's serial port)
-ser = serial.Serial('/dev/cu.usbserial-110', 9600)
+window = tk.Tk()
 
-# Directory to save received files
-output_directory = "LEM_20231010/LEM_6"
+def open_folder():
+    output_directory = fd.askdirectory()
+    # Open a serial connection (modify 'COMX' to your Arduino's serial port)
+    ser = serial.Serial('/dev/cu.usbserial-110', 9600)
 
-# Create the output directory if it doesn't exist
-os.makedirs(output_directory, exist_ok=True)
+    # Directory to save received files
+    # output_directory = "LEM_20231010/LEM_6"
 
-# Initialize an empty NumPy array to accumulate data
-data_array = np.array([], dtype=np.uint8)
+    # Create the output directory if it doesn't exist
+    os.makedirs(output_directory, exist_ok=True)
 
-# line = ser.readline().decode('utf-8').strip()
-# if line == "Start":
-#     start = True
+    # Initialize an empty NumPy array to accumulate data
+    data_array = np.array([], dtype=np.uint8)
 
-ser.write(b'R')
+    # line = ser.readline().decode('utf-8').strip()
+    # if line == "Start":
+    #     start = True
 
-while True:
-    line = ser.readline().decode('utf-8').strip()
-    if line.startswith("Sending file: "):
-        filename = line[14:]
-        with open(output_directory + filename, 'wb') as f:
-            while True:
-                data = ser.readline()
-                newLine = data.decode('utf-8').strip()
-                if newLine.endswith("End of file"):
-                    break
-                f.write(data)
+    ser.write(b'R')
 
-    elif line == "End":
-        break
+    while True:
+        line = ser.readline().decode('utf-8').strip()
+        if line.startswith("Sending file: "):
+            filename = line[14:]
+            with open(output_directory + filename, 'wb') as f:
+                while True:
+                    data = ser.readline()
+                    newLine = data.decode('utf-8').strip()
+                    if newLine.endswith("End of file"):
+                        break
+                    f.write(data)
 
-    else:
-        time.sleep(1)  # Wait for more data
+        elif line == "End":
+            break
 
-ser.close()
+        else:
+            time.sleep(1)  # Wait for more data
+
+    ser.close()
+    
+
+button = tk.Button(text='Refresh', command=open_folder)
+button.pack(padx=50, pady=50)
+
+window.mainloop()
 
 # Now you have all the data in data_array as a NumPy array
