c0e80a001293e03d74b7339646cee68e1737eef9
Ludovic Garon <ludovicgaron54@gmail.com>
2024-06-06 12:23:29 -0400

    updated UI and added exception handling

--- Code/SerialReader/SerialReader.py
+++ Code/SerialReader/SerialReader.py
@@ -1,4 +1,6 @@
 import serial
+import serial.tools.list_ports
+from serial.serialutil import SerialException
 import time
 import datetime as dt
 import os
@@ -6,68 +8,75 @@ import numpy as np
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog as fd
-import serial.tools.list_ports
+import sys
 
 window = tk.Tk()
 window.title("Lemming")
-window.iconbitmap("../../Logo/Logo.ico")
+
+status_label = tk.Label(text='Select the Firebeetle\'s port, it should be the same port as in the Arduino app, then select where to save the data.')
+status_label.pack(ipadx=100, ipady=50)
 
 ports = serial.tools.list_ports.comports()
 
 port_selector = ttk.Combobox(window, textvariable=tk.StringVar())
 port_selector['values'] = ports
-port_selector.pack(ipadx=100, pady=50)
+port_selector.pack(ipadx=100, pady=25)
 
 port_selector.current(0)
 
-status_label = tk.Label(text='Select the Arduino port then select where to save the data.')
-status_label.pack(ipadx=100, ipady=50)
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath("../../Logo/")
+
+    return os.path.join(base_path, relative_path)
+
+
+window.iconbitmap(resource_path("Logo.ico"))
+
 
 def open_folder():
-    output_directory = fd.askdirectory()
-    # Open a serial connection (modify 'COMX' to your Arduino's serial port)
-    ser = serial.Serial(port_selector.get().split(' ')[0], 115200)
+    try:
+        output_directory = fd.askdirectory()
+        # Open a serial connection (modify 'COMX' to your Arduino's serial port)
+        ser = serial.Serial(port_selector.get().split(' ')[0], 115200)
 
-    # Directory to save received files
-    # output_directory = "LEM_20231010/LEM_6"
+        # Create the output directory if it doesn't exist
+        os.makedirs(output_directory, exist_ok=True)
 
-    # Create the output directory if it doesn't exist
-    os.makedirs(output_directory, exist_ok=True)
+        ser.write(b'R')
 
-    # Initialize an empty NumPy array to accumulate data
-    data_array = np.array([], dtype=np.uint8)
+        while True:
+            line = ser.readline().decode().strip()
+            if line.startswith("Sending file: "):
+                filename = line[14:]
+                with open(output_directory + filename, 'wb') as f:
+                    while True:
+                        data = ser.readline()
+                        newLine = data.decode('utf-8').strip()
+                        if newLine.endswith("End of file"):
+                            break
+                        f.write(data)
 
-    # line = ser.readline().decode('utf-8').strip()
-    # if line == "Start":
-    #     start = True
+            elif line == "End":
+                break
 
-    ser.write(b'R')
+            else:
+                time.sleep(1)  # Wait for more data
 
-    while True:
-        line = ser.readline().decode().strip()
-        if line.startswith("Sending file: "):
-            filename = line[14:]
-            with open(output_directory + filename, 'wb') as f:
-                while True:
-                    data = ser.readline()
-                    newLine = data.decode('utf-8').strip()
-                    if newLine.endswith("End of file"):
-                        break
-                    f.write(data)
-
-        elif line == "End":
-            break
-
-        else:
-            time.sleep(1)  # Wait for more data
-
-    ser.close()
-    status_label.config(text=f"File download done at {dt.datetime.now().strftime("%Y/%m/%d %H:%M:%S")}")
+        ser.close()
+        status_label.config(text=f"File download done at {dt.datetime.now().strftime("%Y/%m/%d %H:%M:%S")}")
+    except SerialException as fileNotFound:
+        status_label.config(text=f"There was an error downloading files, you probably didn't select the right port. \n This is the error generated : \n{fileNotFound}")
+    except Exception as e:
+        status_label.config(text=f"There was an error downloading files, this is the error generated : \n{e}")
     
 
 button = tk.Button(text='Select Folder', command=open_folder)
 button.pack(padx=250, pady=50)
 
 window.mainloop()
-
-# Now you have all the data in data_array as a NumPy array
